66c3e299c2108281d5e81d9b5e409ff94101ded6
Ian Cordasco <graffatcolmingov@gmail.com>
2012-12-21 20:06:58 -0500

    Index and some preliminary fixtures

---
+++ manage.py
@@ -0,0 +1,4 @@
+from mock_github_api.core import app
+
+if __name__ == '__main__':
+    app.run()
---
+++ mock_github_api/__init__.py
@@ -0,0 +1,13 @@
+"""
+
+mock_github_api
+~~~~~~~~~~~~~~~
+
+A mock of the GitHub API for testing purposes. Inspired by httpbin.org.
+
+"""
+
+__title__ = 'mock_github_api'
+__author__ = 'Ian Cordasco'
+__version__ = '0.1'
+__copyright__ = 'Copyright 2012 Ian Cordasco'
