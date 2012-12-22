633af265346f898f9c0e2bb9bc7ec382ff883ef9
Ian Cordasco <graffatcolmingov@gmail.com>
2012-12-21 20:22:35 -0500

    And one other forgotten thing.

--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
+import os
 from mock_github_api.core import app
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
