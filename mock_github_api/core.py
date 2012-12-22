0fc21ccf37c79f8262f590e932d951593e86d030
Ian Cordasco <graffatcolmingov@gmail.com>
2012-12-21 21:25:55 -0500

    Add two end points

--- mock_github_api/core.py
+++ mock_github_api/core.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from mock_github_api.helpers import response_from_fixture
 
 app = Flask(__name__)
 
@@ -8,3 +9,19 @@ ROBOTS = """User-agent: *"""
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/user')
+def auth_user():
+    response = response_from_fixture('user')
+    if response.data is '':
+        response.status_code = 404
+    return response
+
+
+@app.route('/users/<login>')
+def get_user(login):
+    response = response_from_fixture('user')
+    if response.data is '':
+        response.status_code = 404
+    return response
