6e83b5ca3090201757b02f47c17e6716f25d2255
Ian Cordasco <graffatcolmingov@gmail.com>
2012-12-24 16:17:29 -0500

    Emulate authentication

--- mock_github_api/helpers.py
+++ mock_github_api/helpers.py
@@ -21,21 +21,38 @@ def get_fixture(name, array=False):
     return ''
 
 
+def _json_response():
+    response = make_response()
+    response.content_type = 'application/json; charset=utf-8'
+    response.mimetype = 'application/json'
+    return response
+
+
 def response_from_fixture(name, array=False, status_code=404):
     """Make a response from a fixture."""
-    response = make_response()
+    response = _json_response()
     response.data = get_fixture(name, array)
     if response.data is '':
         response.status_code = status_code
-    response.content_type = 'application/json; charset=utf-8'
-    response.mimetype = 'application/json'
     return response
 
 
 def boolean_response(status_code):
     """Make a response with no data, only a status code."""
-    response = make_response()
-    response.content_type = 'application/json; charset=utf-8'
-    response.mimetype = 'application/json'
+    response = _json_response()
     response.status_code = status_code
     return response
+
+
+def not_authorized_response():
+    """Respond that the user is not authorized."""
+    response = _json_response()
+    response.status_code = 401
+    response.data = '{"message": "Requires authentication"}'
+    return response
+
+
+def is_authorized(request):
+    """Check to see if a request has an authorization header or has the
+    client_id/client_secret parameters in the query string."""
+    return request.authorization is not None
--- mock_github_api/user.py
+++ mock_github_api/user.py
@@ -1,40 +1,56 @@
+from flask import request
 from mock_github_api.core import app
-from mock_github_api.helpers import response_from_fixture, boolean_response
+from mock_github_api.helpers import (response_from_fixture, boolean_response,
+                                     is_authorized, not_authorized_response)
 
 
 @app.route('/user', methods=['GET', 'PATCH'])
 def user():
+    if not is_authorized(request):
+        return not_authorized_response()
     return response_from_fixture('user', status_code=404)
 
 
 @app.route('/user/emails', methods=['GET', 'POST', 'DELETE'])
 def emails():
+    if not is_authorized(request):
+        return not_authorized_response()
     return response_from_fixture('email', True, 404)
 
 
 @app.route('/user/followers')
 @app.route('/user/following')
 def iter_following():
+    if not is_authorized(request):
+        return not_authorized_response()
     return response_from_fixture('user', True, 404)
 
 
 @app.route('/user/issues')
 def issues():
+    if not is_authorized(request):
+        return not_authorized_response()
     return response_from_fixture('issue', True, 404)
 
 
 @app.route('/user/keys/<id>')
 def key(id):
+    if not is_authorized(request):
+        return not_authorized_response()
     return response_from_fixture('key', status_code=404)
 
 
 @app.route('/user/keys')
 def keys():
+    if not is_authorized(request):
+        return not_authorized_response()
     return response_from_fixture('key', True, 404)
 
 
 @app.route('/user/orgs')
 def orgs():
+    if not is_authorized(request):
+        return not_authorized_response()
     return response_from_fixture('org', True, 404)
 
 
@@ -42,15 +58,21 @@ def orgs():
 @app.route('/user/starred')
 @app.route('/user/subscriptions')
 def repos():
+    if not is_authorized(request):
+        return not_authorized_response()
     return response_from_fixture('repo', True, 404)
 
 
 @app.route('/user/starred/<login>/<repo>', methods=['DELETE', 'PUT'])
 @app.route('/user/subscriptions/<login>/<repo>', methods=['DELETE', 'PUT'])
 def star_subscribe(login, repo):
+    if not is_authorized(request):
+        return not_authorized_response()
     return boolean_response(204)
 
 
 @app.route('/user/following/<login>', methods=['DELETE'])
 def unfollow(login):
+    if not is_authorized(request):
+        return not_authorized_response()
     return boolean_response(204)
